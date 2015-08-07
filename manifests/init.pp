ccb8383ee154103d457e8b364241b2f79371d3db
Russ Johnson <russ@unifiedsocial.com>
2015-08-07 14:27:07 -0700

    cannot reassign variable name

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,10 @@
 define unfester (
   $path,
-  $name        = $title,
   $cut_path    = '/bin/cut',
   $chattr_path = '/usr/bin/chattr',
   $lsattr_path = '/usr/bin/lsattr'
   ) {
-  exec { "${name}":
+  exec { "${title}":
     command => "${chattr_path} -i ${path}",
     onlyif  => "[ `${lsattr_path} ${path} | ${cut_path} -c5` = 'i' ]",
   }
